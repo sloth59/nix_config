c39947dd2981ed3bb65c46b1ecae4246de9e9310
sloth59 <69042488+sloth59@users.noreply.github.com>
2021-01-27 03:54:31 +0000

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,127 +1,147 @@
 { config, pkgs, ... }:
 
 {
-        imports =
-        [ # Include the results of the hardware scan.
-                ./hardware-configuration.nix
-        ];
+	imports =
+    	[       # Include the results of the hardware scan.
+      		./hardware-configuration.nix
+    	];
 
 
-        # Use the systemd-boot EFI boot loader.
-        boot = {
-                loader = {
-                        systemd-boot.enable = true;
-                        efi.canTouchEfiVariables = true;
-                        grub.useOSProber = true;
-                };
+	# Use the systemd-boot EFI boot loader.
+  	boot = {
+		loader = {
+			systemd-boot.enable = true;
+  			efi.canTouchEfiVariables = true;
+  			grub.useOSProber = true;
+		};
+                
+		supportedFilesystems = [ "ntfs" ];
+		plymouth.enable = true; # Enable boot splash screen
+	};
+	
+	
+  	networking = {
+		hostName = "Nix"; # Define your hostname.
+  		networkmanager.enable = true; # Enables wireless
+	 	
+		# replicates the default behaviour.
+  		useDHCP = false;
+  		interfaces = {
+  			enp3s0.useDHCP = true;
+  			wlp4s0.useDHCP = true;
+  		};
+	};
+	
+	
+  	# Set your time zone.
+  	time.timeZone = "Asia/Kathmandu";
+  	
+  	
+  	# Select internationalisation properties.
+  	i18n.defaultLocale = "en_US.UTF-8";
+  	console = {
+    		font = "Lat2-Terminus16";
+    		keyMap = "us";
+  	};
+  	
+  	
+ 	# Define a user account.
+  	users.users.sloth = {
+    		isNormalUser = true;
+    		extraGroups = [ "wheel" "networkmanager" ];
+  	};
+  	
+  	
+  	# Enable sound.
+  	sound.enable = true;
+  	hardware = {
+  		pulseaudio = {
+  			enable = true;
+  			# package = pkgs.pulseaudioFull;
+  		};
+  	};
+  	
 
-                supportedFilesystems = [ "ntfs" ];
+	# Enable non-free softwre
+	# nixpkgs.config.allowUnfree = true;
 
-                plymouth.enable = true;
+  	# List services that you want to enable:
+  	services = {
+  	  	# Enable Xserver.
+		xserver = {
+			enable = true;
+			displayManager = {
+				lightdm = {
+					enable = true;
+					greeters.enso.enable = true;
+				};
+			};
+			desktopManager = {
+				mate.enable = true;
+			};	
+	
+  			# Configure keymap in X11
+  			layout = "us";
+			# xkbOptions = "eurosign:e";
+  		
+			# Enable touchpad support
+  			libinput.enable = true;	
+		};
 
-        };
+		# Enable picom for graphical effects in windowManagers
+		/*picom = {
+			enable = true;
+			fade = true;
+			inactiveOpacity = 0.9;
+			shadow = true;
+			fadeDelta = 4;
+		};*/
+		
+  		# Enable CUPS to print documents.
+  		# printing.enable = true;
+ 	
+  		# Enable the OpenSSH daemon.
+  		openssh.enable = true;
+	};
+	
+	
+  	# Fonts
+  	fonts.fonts = with pkgs; [
+    	        dejavu_fonts
+    	        inconsolata
+    	        font-awesome
+  	];
+  	
+  	
+  	# List packages installed in system profile. To search, run:
+  	# $ nix search wget
+  	environment.systemPackages = with pkgs; [
+    	        tmux vim
+    	        wget curl lynx git
+	        file unzip tree
+	    	
+    	        brightnessctl
+    	        cmus cava
+    	        neofetch
 
+    	        gcc gdb valgrind
+	        emacs vscodium
+	
+    	        firefox chromium
+    	        vlc
+    	        transmission-gtk
+    	        libreoffice
+    	        gimp
+    	        flameshot
+    	        openshot-qt
+    	        obs-studio
+	
+	        gparted
 
-        networking = {
-                hostName = "Nix"; # Define your hostname.
-                networkmanager.enable = true; # Enables wireless
-         
-                # replicates the default behaviour.
-                useDHCP = false;
-                interfaces = {
-                        enp3s0.useDHCP = true;
-                        wlp4s0.useDHCP = true;
-                };
-
-        };
-
-
-        # Set your time zone.
-        time.timeZone = "Asia/Kathmandu";
-  
-  
-        # Select internationalisation properties.
-        i18n.defaultLocale = "en_US.UTF-8";
-        console = {
-        font = "Lat2-Terminus16";
-        keyMap = "us";
-        };
-  
-  
-        # Define a user account.
-        users.users.sloth = {
-        isNormalUser = true;
-        extraGroups = [ "wheel" "networkmanager" ]; # Enable ‘sudo’ for the user.
-        };
-  
-  
-        # Enable sound.
-        sound.enable = true;
-        hardware = {
-                pulseaudio = {
-                        enable = true;
-                        # package = pkgs.pulseaudioFull;
-                };
-        };
-  
-  
-        # List services that you want to enable:
-        services = {
-                # Enable Xserver.
-                xserver = {
-                        enable = true;
-                        displayManager.sddm.enable = true;
-                        desktopManager.plasma5.enable = true;
-
-                        # Configure keymap in X11
-                        layout = "us";
-                        # xkbOptions = "eurosign:e";
-  
-                        # Enable touchpad support (enabled default in most desktopManager).
-                        libinput.enable = true;
-                };
-
-                # Enable CUPS to print documents.
-                # printing.enable = true;
- 
-                # Enable the OpenSSH daemon.
-                openssh.enable = true;
-        };
-
-
-        # Fonts
-        fonts.fonts = with pkgs; [
-        dejavu_fonts
-        inconsolata
-        font-awesome
-        ];
-  
-  
-        # List packages installed in system profile. To search, run:
-        # $ nix search wget
-        environment.systemPackages = with pkgs; [
-        vim
-        wget curl git tmux lynx file tree unzip
-
-        gcc gdb valgrind
-    
-        brightnessctl
-        cmus cava
-        neofetch
-
-        firefox
-        vlc
-        transmission-gtk
-        libreoffice
-        gimp
-        # flameshot
-        openshot-qt
-        obs-studio
-
-        whois nmap wireshark
-        python38Packages.binwalk steghide
-        ];
-  
-  
-        system.stateVersion = "20.09";
+    	        whois nmap wireshark
+    	        python38Packages.binwalk steghide
+  	];
+  	
+  	
+  	system.stateVersion = "20.09";
 }
